fd40faee6e53dca790429dc5e50fe2f50a460fab
Alec-NK <alec.arquivos@gmail.com>
2022-02-08 20:13:13 -0300

    fix: removendo arquivos

--- src/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-const app_port = 3000;
-
-app.listen(app_port, function() {
-    console.log("Server running at port: " + app_port);
-});
